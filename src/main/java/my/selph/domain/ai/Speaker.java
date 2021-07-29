485180fe1ab7afc7b076404d77acb3142d8e1c62
Owen Grant <owen.dalmon.grant@gmail.com>
2021-07-28 20:14:21 -0400

    add support for text to speech

---
+++ src/main/java/my/selph/domain/ai/Speaker.java
@@ -0,0 +1,28 @@
+package my.selph.domain.ai;
+
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.SingleFileAudioPlayer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.sound.sampled.AudioFileFormat;
+
+@ApplicationScoped
+public class Speaker {
+
+    public void toAudio(String text) {
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        var voice = voiceManager.getVoice("kevin16");
+        voice.allocate();
+        var audioPlayer = new SingleFileAudioPlayer("output123", AudioFileFormat.Type.WAVE);
+        voice.setAudioPlayer(audioPlayer);
+        voice.setRate(150);
+        voice.setPitch(150);
+        voice.setVolume(3);
+        voice.speak(text);
+        voice.deallocate();
+        audioPlayer.close();
+    }
+
+}
